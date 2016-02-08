2bf608a92aea316993bafbc99012b07b7c0cc5be
Andrea Zoli <zoli@iasfbo.inaf.it>
2016-02-08 18:46:26 +0100

    Add AG_summapgen5.

---
+++ code/AG_summapgen5.cpp
@@ -0,0 +1,97 @@
+////////////////////////////////////////////////////////////////////////////////
+// Date: Feb 2016
+// Authors: Andrea Zoli (IASF_Bologna)
+//
+// INPUT
+//       A maplist4, with multiple cts and exposure maps.
+//
+// OUTPUT
+//       Gives a cts and an exp map computed respectively as the sum of cts maps and the sum of exp maps.
+//
+// NOTICE
+//       Any information contained in this software
+//       is property of the AGILE TEAM and is strictly
+//       private and confidential.
+//       All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <cstdlib>
+#include <PilParams.h>
+#include <AgileMap.h>
+#include <AlikeData5.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+const char* startString = {
+"###################################################\n"
+"### Task AG_summapgen5 started ####################"
+};
+
+const char* endString = {
+"### Task AG_summapgen5 ended successfully #########\n"
+"###################################################"
+};
+
+const PilDescription paramsDescr[] = {
+    { PilString, "maplist", "Input maplist" },
+    { PilString, "outprefix", "Output name prefix for the maps" },
+    { PilNone, "", "" }
+};
+
+int main(int argc, char *argv[]) {
+    cout << startString << endl;
+
+    PilParams params(paramsDescr);
+    if (!params.Load(argc, argv))
+        return EXIT_FAILURE;
+
+    cout << endl << "INPUT PARAMETERS:" << endl;
+    params.Print();
+
+    MapList maplist;
+    int mapCount = maplist.Read(params["maplist"]);
+    if (!mapCount) {
+        cerr << "File " << params.GetStrValue("maplist") << " missing or empty." << endl;
+        return EXIT_FAILURE;
+    }
+    MapData mapData;
+    if (!mapData.Load(maplist)) {
+        cerr << "Error loading map data from " << params.GetStrValue("maplist") << "." << endl;
+        return EXIT_FAILURE;
+    }
+
+    AgileMap sumCts = mapData.CtsMap(0);
+    AgileMap sumExp = mapData.ExpMap(0);
+    double emin = sumCts.GetEmin();
+    double emax = sumCts.GetEmax();
+    for(int i=1; i<mapData.Count(); i++) {
+        AgileMap otherCts = mapData.CtsMap(i);
+        for (int y=0; y<sumCts.Dim(0); ++y)
+            for (int x=0; x<sumCts.Dim(1); ++x)
+                sumCts(y, x) += otherCts(y, x);
+        AgileMap otherExp = mapData.ExpMap(i);
+        for (int y=0; y<sumExp.Dim(0); ++y)
+            for (int x=0; x<sumExp.Dim(1); ++x)
+                sumExp(y, x) += otherExp(y, x);
+        if(otherCts.GetEmin() < emin)
+            emin = otherCts.GetEmin();
+        if(otherCts.GetEmax() > emax)
+            emax = otherCts.GetEmax();
+    }
+    sumCts.SetEnergy(emin, emax);
+    sumExp.SetEnergy(emin, emax);
+
+    std::string outCts = std::string(params["outprefix"])+".cts.gz";
+    if(sumCts.Write(outCts.c_str()))
+        return EXIT_FAILURE;
+
+    std::string outExp = std::string(params["outprefix"])+".exp.gz";
+    if(sumExp.Write(outExp.c_str()))
+        return EXIT_FAILURE;
+
+    cout << endString << endl;
+    return EXIT_SUCCESS;
+}

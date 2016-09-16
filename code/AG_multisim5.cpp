02c2b3f49920affde36c664f2d5aab82644171fa
Andrea Zoli <zoli@iasfbo.inaf.it>
2016-09-16 13:56:40 +0200

    AG_multisim5: Generate the gas map.

--- code/AG_multisim5.cpp
+++ code/AG_multisim5.cpp
@@ -1,41 +1,34 @@
-////////////////////////////////////////////////////////////////////////////////
-// DESCRIPTION
-//       Scientific pipeline I/O routine
-//       AG_multisim
-//       Release: BUILD 21 -  30/Dec/2010
-//       Contributors: 
-//       Author: Andrew Chen, Tomaso Contessi (IASF-Milano)
-//
-// INPUT
-//       TBD
-//
-// OUTPUT
-//       TBD
-//
-//
-// FILE HISTORY
-//       30/Dec/2010
-//                      First release: V0.1
-//       		Author: Andrew Chen, Tomaso Contessi (IASF-Milano)
-// NOTICE
-//       Any information contained in this software
-//       is property of the AGILE TEAM and is strictly
-//       private and confidential.
-//       All rights reserved.
-////////////////////////////////////////////////////////////////////////////////////
-
-
+/*
+ * Copyright (c) 2010-2016
+ *     Andrew Chen, Tommaso Contessi (IASF-Milano),
+ *     Andrea Bulgarelli, Andrea Zoli (IASF-Bologna),
+ *
+ * Any information contained in this software
+ * is property of the AGILE TEAM and is strictly
+ * private and confidential.
+*/
 
 #include <TRandom3.h>
+#include <RoiMulti5.h>
+#include <PilParams.h>
+#include <Eval.h>
+#include <FitsUtils.h>
 
-#include "RoiMulti5.h"
-#include "PilParams.h"
+using std::cout;
+using std::cerr;
+using std::endl;
 
+const char* startString = {
+"#################################################################\n"
+"### Task AG_multisim5 v1.2.0 - A.C., T.C., A.B., A.Z.         ###"
+};
 
-using namespace std;
+const char* endString = {
+"### Task AG_multisim5 exiting ................................ ###\n"
+"##################################################################"
+};
 
-
-const PilDescription c_params[] = {
+const PilDescription paramsDescr[] = {
 	{ PilInt,    "opmode", "Operation Mode" },
 	{ PilInt,    "block", "Block" },
 	{ PilInt,    "nruns", "Number of runs" },
@@ -43,12 +36,9 @@ const PilDescription c_params[] = {
 	{ PilString, "sarfile", "SAR file name" },
 	{ PilString, "edpfile", "EDP file name" },
 	{ PilString, "psdfile", "PSD file name" },
-
-
 	{ PilString, "maplistsim", "Map list for simulation" },
 	{ PilString, "srclistsim", "Source list for simulation" },
 	{ PilString, "outfile", "Output file name" },
-
 	{ PilString, "maplistanalysis", "Map list for analysis" },
 	{ PilString, "srclistanalysis", "Source list for analysis" },
 	{ PilReal,   "ranal", "Radius of analysis" },
@@ -56,335 +46,149 @@ const PilDescription c_params[] = {
 	{ PilInt,    "isomode", "Isotropic emission mode" },
 	{ PilReal,   "ulcl",    "Upper limit confidence level" },
 	{ PilReal,   "loccl",   "Location contour confidence level" },
+	{ PilString, "diffusefile", "Diffuse model filename" },
+	{ PilString, "hiresdiffusefile", "High res diffuse model filename" },
 	{ PilNone,   "",   "" }
-	};
-
-
-class MultiSimParams: public PilParams
-{
-public:
-	MultiSimParams(): PilParams(c_params) {}
-	void Print() { PilParams::Print(c_params); }
 };
-
 
 enum { Concise=1, SkipAnalysis=2, DoubleAnalysis=4, SaveMaps=8 };
 
+AgileMap SumMaps(const AgileMap* mapArr, int offset, int count) {
+	AgileMap m(mapArr[offset]);
+	for (int i=offset+1; i<offset+count; ++i)
+		m += mapArr[i];
+	return m;
+}
 
-/**
-static int DoPIL(
-	int argc,
-	char *argv[],
-
-	int& opmode,
-	int& block,
-	int& nruns,
-	int& seed,
-
-	char* sarfilename,
-	char* edpfilename,
-	char* psdfilename,
-
-	char* maplistsimname,
-	char* srclistsim,
-	char* outfilename,
-
-	char* maplistanalysisname,
-	char* srclistanalysis,
-	double& ranal,
-	int& galmode,
-	int& isomode,
-	double& ulcl,
-	double& loccl)
-{
-int status = PILInit(argc, argv);
-int numpar = 0;
-status = status || PILGetNumParameters(&numpar);
-
-GetPilParam("opmode", &opmode, &status);
-GetPilParam("block", &block, &status);
-if (block<0)
-	block = -block;
-GetPilParam("nruns", &nruns, &status);
-GetPilParam("seed", &seed, &status);
-
-GetPilParam("sarfile", sarfilename, &status);
-GetPilParam("edpfile", edpfilename, &status);
-GetPilParam("psdfile", psdfilename, &status);
-
-GetPilParam("maplistsim", maplistsimname, &status);
-GetPilParam("srclistsim", srclistsim, &status);
-
-GetPilParam("outfile", outfilename, &status);
-
-if ((opmode&SkipAnalysis)==0) {
-	GetPilParam("maplistanalysis", maplistanalysisname, &status);
-	GetPilParam("srclistanalysis", srclistanalysis, &status);
-	GetPilParam("ranal", &ranal, &status);
-	GetPilParam("galmode", &galmode, &status);
-	GetPilParam("isomode", &isomode, &status);
-	GetPilParam("ulcl", &ulcl, &status);
-	GetPilParam("loccl", &loccl, &status);
-	}
-
-PILClose(status);
-return status;
+AgileMap SumExposure(const MapMaps& maps, int offset, int count) {
+	AgileMap m(maps.ExpMap(offset));
+	for (int i=offset+1; i<offset+count; ++i)
+		m += maps.ExpMap(i);
+	return m;
 }
 
 
-static void PrintDiffMode(int mode)
-{
-cout << mode;
-if (mode==0)
-	cout << " (constant)" << endl;
-else if (mode==1)
-	cout << " (from file)" << endl;
-else if (mode==2)
-	cout << " (variable)" << endl;
-else if (mode==3)
-	cout << " (single variable parameter)" << endl;
-}
+int main(int argc,char **argv) {
+	cout << startString << endl;
 
+	PilParams params(paramsDescr);
+	if (!params.Load(argc, argv))
+		return EXIT_FAILURE;
 
-static void PrintInput(
-	int opmode,
-	int block,
-	int nruns,
-	int seed,
+	cout << endl << "INPUT PARAMETERS:" << endl;
+	params.Print();
 
-	const char* sarfilename,
-	const char* edpfilename,
-	const char* psdfilename,
-
-	const char* maplistsimname,
-	const char* srclistsim,
-
-	const char* outfilename,
-
-	const char* maplistanalysisname,
-	const char* srclistanalysis,
-	double ranal,
-	int galmode,
-	int isomode,
-	double ulcl,
-	double loccl)
-{
-cout << endl << endl << "INPUT PARAMETERS:"<< endl << endl;
-
-cout << "Operation mode: " << opmode;
-if (opmode & Concise)
-	cout << " Concise output";
-if (opmode & SkipAnalysis)
-	cout << " No analysis performed";
-else if (opmode & DoubleAnalysis)
-	cout << " Double Analysis performed";
-if (opmode & SaveMaps)
-	cout << " Intermediate maps saved";
-if (opmode==0)
-	cout << " Verbose output";
-cout << endl;
-
-cout << "Block: " << block;
-if (block==0)
-	cout << " (No Block iteration)" << endl;
-else
-	cout << " (Block iteration)" << endl;
-
-cout << "Number of runs: " << nruns << endl;
-
-if (seed)
-	cout << "Seed: " << seed << endl;
-else
-	cout << "Seed: Random (" << GetSeed() << ")" << endl;
-
-cout << endl << "Calibration" << endl;
-cout << "SAR file name: " << sarfilename << endl;
-cout << "EDP file name: " << edpfilename << endl;
-cout << "PSD file name: " << psdfilename << endl;
-
-cout << endl << "Simulation" << endl;
-cout << "Simulation Map list: " << maplistsimname << endl;
-cout << "Simulation Source list: " <<  srclistsim << endl;
-cout << "Output file prefix: " <<  outfilename << endl;
-
-cout << endl << "Analysis";
-if (opmode&SkipAnalysis)
-	cout << " not performed" << endl;
-else {
-	cout << endl;
-	cout << "Analysis Map list: " << maplistanalysisname << endl;
-	cout << "Analysis Source list: " <<  srclistanalysis << endl;	
-	cout << "Radius of analysis: " << ranal << endl;
-	cout << "Galactic parameter mode: "; PrintDiffMode(galmode);
-	cout << "Isotropic parameter mode: "; PrintDiffMode(isomode);
-	cout << "Upper limit confidence level: " << ulcl << endl;
-	cout << "Location contour confidence level: " << loccl << endl;
-	}
-cout << endl;
-}
-*/
-
-
-class AppScreen
-{
-public:
-	AppScreen()
-	{
-	cout << "#################################################################"<< endl;
-	cout << "###### AG_multisim5  v1.2.0 - A.C., T.C.                    #####"<< endl;
-	cout << "#################################################################"<< endl;
-	cout << endl << "RoiMulti version: " << ROIMULTI_VERSION << " - " << ROIMULTI_DATE<< endl;
-	}
-
-	~AppScreen()
-	{
-	cout << "#################################################################"<< endl;
-	cout << "##########  Task AG_multisim5.......... exiting #################"<< endl;
-	cout << "#################################################################"<< endl;
-	}
-};
-
-
-#define AlikeMap AgileMap
-/**
-AlikeMap SumMaps(const AlikeMap* mapArr, int offset, int count)
-{
-AlikeMap m(mapArr[offset]);
-for (int i=offset+1; i<offset+count; ++i)
-	m.TMatrixD::operator+=(mapArr[i]);
-return m;
-}
-
-AlikeMap SumExposure(const MapMaps& maps, int offset, int count)
-{
-AlikeMap m(maps.ExpMap(offset));
-for (int i=offset+1; i<offset+count; ++i)
-	m.TMatrixD::operator+=(maps.ExpMap(i));
-return m;
-}
-*/
-AgileMap SumMaps(const AgileMap* mapArr, int offset, int count)
-{
-AgileMap m(mapArr[offset]);
-for (int i=offset+1; i<offset+count; ++i)
-	m += mapArr[i];
-return m;
-}
-
-AgileMap SumExposure(const MapMaps& maps, int offset, int count)
-{
-AgileMap m(maps.ExpMap(offset));
-for (int i=offset+1; i<offset+count; ++i)
-	m += maps.ExpMap(i);
-return m;
-}
-
-
-int main(int argc,char **argv)
-{
-AppScreen appScreen;
-
-
-MultiSimParams mPars;
-if (!mPars.Load(argc, argv))
-	return -1;
-cout << endl << endl << "INPUT PARAMETERS:" << endl << endl;
-mPars.Print();
-
-int opmode = mPars["opmode"];
-int block = mPars["block"];
-int nruns = mPars["nruns"];
-int seed = mPars["seed"];
-const char* sarfilename = mPars["sarfile"];
-const char* edpfilename = mPars["edpfile"];
-const char* psdfilename = mPars["psdfile"];
-const char* maplistsimname = mPars["maplistsim"];
-const char* srclistsim = mPars["srclistsim"];
-const char* outfilename = mPars["outfile"];
-const char* maplistanalysisname = mPars["maplistanalysis"];
-const char* srclistanalysis = mPars["srclistanalysis"];
-
-
-
-double ranal = mPars["ranal"];
-int galmode = mPars["galmode"];
-int isomode = mPars["isomode"];
-double ulcl = mPars["ulcl"];
-double loccl = mPars["loccl"];
-
+	int opmode = params["opmode"];
+	int block = params["block"];
+	int nruns = params["nruns"];
+	int seed = params["seed"];
+	const char* sarfilename = params["sarfile"];
+	const char* edpfilename = params["edpfile"];
+	const char* psdfilename = params["psdfile"];
+	const char* maplistsimname = params["maplistsim"];
+	const char* srclistsim = params["srclistsim"];
+	const char* outfilename = params["outfile"];
+	const char* maplistanalysisname = params["maplistanalysis"];
+	const char* srclistanalysis = params["srclistanalysis"];
+	const char* diffusefile = params["diffusefile"];
+	const char* hiresdiffusefile = params["hiresdiffusefile"];
+	double ranal = params["ranal"];
+	int galmode = params["galmode"];
+	int isomode = params["isomode"];
+	double ulcl = params["ulcl"];
+	double loccl = params["loccl"];
 
 	if (seed)
 		SetSeed(seed);
 
 	MapList maplistsim;
-	if (!maplistsim.Read(maplistsimname))
+	if (!maplistsim.Read(maplistsimname)) {
+		cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+		cout << endString << endl;
 		return -1;
-	MapList maplistanalysis;
-	if ((opmode&SkipAnalysis)==0)
-		if (!maplistanalysis.Read(maplistanalysisname))
-			return -1;
+	}
 
-	if (block>0 && (opmode&SkipAnalysis)==0)
-		if (maplistsim.Count()!=maplistanalysis.Count()) {
-			cerr << "ERROR: The two map lists must have the same number of rows" << endl;
+	MapList maplistanalysis;
+	if ((opmode&SkipAnalysis)==0) {
+		if (!maplistanalysis.Read(maplistanalysisname)) {
+			cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+			cout << endString << endl;
 			return -1;
-			}
+		}
+	}
+
+	if (block>0 && (opmode&SkipAnalysis)==0) {
+		if (maplistsim.Count()!=maplistanalysis.Count()) {
+			cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+			cout << "ERROR: The two map lists must have the same number of rows" << endl;
+			cout << endString << endl;
+			return -1;
+		}
+	}
 	if (block>maplistsim.Count())
 		block = maplistsim.Count();
 
 	MapData mapData;
 	if (!mapData.Load(maplistsim, true)) {
-		cerr << "Error reading the simulation map list" << endl;
+		cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+		cout << "Error reading the simulation map list" << endl;
+		cout << endString << endl;
 		return -1;
-		}
+	}
 	MapData mapDataAna;
-	if ((opmode&SkipAnalysis)==0)
+	if ((opmode&SkipAnalysis)==0) {
 		if (!mapDataAna.Load(maplistanalysis, true)) {
-			cerr << "Error reading the analysis map list" << endl;
+			cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+			cout << "Error reading the analysis map list" << endl;
+			cout << endString << endl;
 			return -1;
-			}
+		}
+	}
 
 	SourceDataArray srcSimArr = ReadSourceFile(srclistsim);
 	SourceDataArray srcAnaArr;
 	if ((opmode&SkipAnalysis)==0)
 		srcAnaArr = ReadSourceFile(srclistanalysis);
 
-/// #define _SINGLE_INSTANCE_
+	/// #define _SINGLE_INSTANCE_
 #ifdef _SINGLE_INSTANCE_
-cout << "Single RoiMulti instance" << endl;
-	RoiMulti roiMulti;
-	if (!roiMulti.SetPsf(psdfilename, sarfilename, edpfilename)) {
-		cerr << "ERROR setting PSF data" << endl;
-		return -1;
+	cout << "Single RoiMulti instance" << endl;
+		RoiMulti roiMulti;
+		if (!roiMulti.SetPsf(psdfilename, sarfilename, edpfilename)) {
+			cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+			cout << "ERROR setting PSF data" << endl;
+			cout << endString << endl;
+			return -1;
 		}
 #else
-cout << "Multiple RoiMulti instance" << endl;
+	cout << "Multiple RoiMulti instance" << endl;
 #endif
 
 	double sumTS = 0;
-	int    analysisCount = 0;
+	int analysisCount = 0;
 
 	for (int i=0; i<nruns; ++i) {
-
 #ifndef _SINGLE_INSTANCE_
 		RoiMulti roiMulti;
 		if (!roiMulti.SetPsf(psdfilename, sarfilename, edpfilename)) {
-			cerr << "ERROR setting PSF data" << endl;
+			cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+			cout << "ERROR setting PSF data" << endl;
+			cout << endString << endl;
 			return -1;
-			}
+		}
 #endif
 		cout << endl << "AG_Multisim loop #" << i+1 << endl << endl;
 
 		mapData.MapCoeff::Load(maplistsim);
 		roiMulti.SetMaps(mapData);
-		AlikeMap* simArr = roiMulti.NewSimulationArray(srcSimArr);
+		AgileMap* simArr = roiMulti.NewSimulationArray(srcSimArr);
 
 		if (block) {
 			int last = mapData.Length()-block;
 			for (int j=0; j<=last; ++j) {
 				cout << endl << "AG_Multisim summing maps from " << j+1 << " to " << j+block << " [loop " << i+1 << "]" << endl << endl;
-				AlikeMap ctsMap = SumMaps(simArr, j, block);
-				AlikeMap expMap = SumExposure(mapData, j, block);
+				AgileMap ctsMap = SumMaps(simArr, j, block);
+				AgileMap expMap = SumExposure(mapData, j, block);
 				/// Writing cts and exp maps
 				if (opmode & SaveMaps) {
 					char mapName[256];
@@ -398,12 +202,19 @@ cout << "Multiple RoiMulti instance" << endl;
 						cerr << "Error writing simulated counts map " << mapName << endl;
 					else
 						cerr << mapName << " written" << endl;
-					}
+				}
 
 				if ((opmode&SkipAnalysis)==0) {
-					// TODO status = eval::EvalGasMap(gasMap, expMap, lowdiffuse, hidiffuse);
+					AgileMap gasMap;
+					int status = eval::EvalGasMap(gasMap, expMap, diffusefile, hiresdiffusefile);
+					if(status) {
+						cout << "Error during gas map evaluation" << endl;
+						cout << "AG_multisim5..................... exiting AG_multisim5 ERROR:" << endl;
+						fits_report_error(stdout, status);
+						return status;
+					}
 					cout << endl << "AG_Multisim: Analysis step" << endl << endl;
-					MapData analysisMaps(ctsMap, expMap, mapDataAna.GasMap(0), 0, 1, 1);
+					MapData analysisMaps(ctsMap, expMap, gasMap, 0, 1, 1);
 					analysisMaps.MapCoeff::Load(maplistanalysis);
 					roiMulti.SetMaps(analysisMaps, galmode, isomode);
 					roiMulti.DoFit(srcAnaArr, ranal, ulcl, loccl);
@@ -411,7 +222,7 @@ cout << "Multiple RoiMulti instance" << endl;
 						cout << endl << "AG_Multisim: Second analysis step" << endl << endl;
 						SourceDataArray newSources = roiMulti.GetFitData();
 						roiMulti.DoFit(newSources, ranal, ulcl, loccl);
-						}
+					}
 
 					++analysisCount;
 					SourceDataArray results = roiMulti.GetFitData();
@@ -427,12 +238,11 @@ cout << "Multiple RoiMulti instance" << endl;
 						sprintf(fileName, "%010d_%03d_%s", i+1, j+1, outfilename);
 						roiMulti.Write(fileName);
 						roiMulti.WriteSources(fileName, true, true);
-						}
 					}
 				}
 			}
+		}
 		else {
-
 			if (opmode & SaveMaps) {
 				char ctsName[256];
 				for (int m=0; m<mapData.Length(); ++m) {
@@ -441,9 +251,8 @@ cout << "Multiple RoiMulti instance" << endl;
 						cerr << "Error writing simulated counts map " << ctsName << endl;
 					else
 						cerr << ctsName << " written" << endl;
-					}
 				}
-
+			}
 			if ((opmode&SkipAnalysis)==0) {
 				cout << endl << "AG_Multisim: Analysis step" << endl << endl;
 				mapDataAna.ReplaceCtsMaps(simArr);
@@ -455,7 +264,7 @@ cout << "Multiple RoiMulti instance" << endl;
 					cout << endl << "AG_Multisim: Second analysis step" << endl << endl;
 					SourceDataArray newSources = roiMulti.GetFitData();
 					roiMulti.DoFit(newSources, ranal, ulcl, loccl);
-					}
+				}
 
 				++analysisCount;
 				SourceDataArray results = roiMulti.GetFitData();
@@ -471,15 +280,16 @@ cout << "Multiple RoiMulti instance" << endl;
 					sprintf(fileName, "%010d_%s", i+1, outfilename);
 					roiMulti.Write(fileName);
 					roiMulti.WriteSources(fileName, true, true);
-					}
 				}
 			}
-		delete[] simArr;
 		}
+		delete[] simArr;
+	}
 	if (analysisCount)
 		cout << endl << "AG_Multisim performed the analysis " << analysisCount << " times" << endl << "Total TS: " << sumTS << ", average: " << sumTS/analysisCount << endl << endl;
 
+	cout << endString << endl;
 
-return 0;
+	return 0;
 }
 

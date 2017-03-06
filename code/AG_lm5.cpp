a47790514f795b56c0c570ccbb1578df622bdaeb
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2017-03-06 15:15:29 +0100

    blind search

--- code/AG_lm5.cpp
+++ code/AG_lm5.cpp
@@ -337,7 +337,7 @@ int main(int argc, char *argv[])
 			cout << "Alpha: 0" << endl;
 			cout << "Li&Ma sigma 0" << endl;
 		}
-		resText << S << endl;
+		resText << " " << S << endl;
 		
 		/*
 		beginTime = endTime;
